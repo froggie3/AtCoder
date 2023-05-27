b329a7a0ee2663cfc8aebe8166a8b2416e3fbe92
Yokkin <yokkin.h@gmail.com>
2023-05-27 23:18:05 +0900

    asd

--- old_php/abcc/202a.php
+++
@@ -1,9 +0,0 @@
-#!/usr/local/bin/php
-<?php
-
-declare(strict_types=1);
-
-fscanf(STDIN, '%d %d %d', $a, $b, $c);
-$arr = array_map(fn ($x) => 7 - $x, [$a, $b, $c]);
-$sum = array_sum($arr);
-echo (string) $sum, PHP_EOL;
