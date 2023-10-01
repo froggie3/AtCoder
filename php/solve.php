a70b63a02d0178b59ca6784bbb7fb5b76711abe8
Yokkin <yokkin.h@gmail.com>
2023-10-02 01:10:36 +0900

    update

---
+++ php/solve.php
@@ -0,0 +1,12 @@
+#!/usr/local/bin/php
+<?php
+
+# [$N] = sscanf(trim(fgets(STDIN)), "%d\n");
+# [$N, $M] = sscanf(trim(fgets(STDIN)), "%d %d\n");
+# $A = array_map("intval", explode(" ", trim(fgets(STDIN))));
+# $S = str_split(trim(fgets(STDIN)));
+# $Q = []; for ($i = 0; $i < $N; $i++) $Q[] = sscanf(trim(fgets(STDIN)), "%d %d\n");
+
+$ans = 0;
+
+echo "$ans\n";
---
+++ solve.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# q = [[*map(int, input().split())] for i in range(N)]
+# A = [*map(int, input().split())]
+# N, M = map(int, input().split())
+# N = int(input())
+
+from collections import *  # Counter, deque, defaultdict
+from itertools import *  # accumulate, permutations, combinations, product
+from bisect import *  # bisect, bisect_left, bisect_right
+from pprint import *
+from math import *  # sin, cos, factorial
